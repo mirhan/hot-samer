b65fe62ed227b82f3e2d437dd05ccf05d1bf4082
mirhan <handachang@gmail.com>
2016-05-11 16:07:55 +0800

    根据日期生成 HTML 名字

--- same_spider/taks.py
+++ same_spider/taks.py
@@ -2,6 +2,7 @@
 # encoding: utf-8
 
 import sys
+import datetime
 
 from same_datas import all_cid_list
 from collect_data_into_es import update_channels
@@ -12,7 +13,11 @@ from template import generate_html
 def generate_last_day_top_html(top_len=50):
     update_channels(all_cid_list)
     samers = get_last_day_top(top_len)
-    generate_html(samers, logfile='last_day_top.html')
+
+    yesterday = datetime.date.today() - datetime.timedelta(1)
+    html_name = yesterday.strftime("%Y-%m-%d") + '.html'
+
+    generate_html(samers, logfile=html_name)
 
 if __name__ == '__main__':
     if sys.argv[1] == 'last_day':
