8882a2804c52cf61f3c8339da8a1fac60f677758
mirhan <handachang@gmail.com>
2016-05-18 10:43:03 +0800

    rename spider list

---
+++ same_spider/d_spider_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from pickle_c import load_p, dump_p
+
+HAS_BEEN = 'has_been.pk'
+TODO_QUEUE = 'todo_queue.pk'
+
+
+def get_has_been():
+    has_been = load_p(filename=HAS_BEEN) or set()
+    return has_been
+
+
+def set_has_been(urls):
+    if urls:
+        dump_p(urls, filename=HAS_BEEN)
+
+
+def get_todo_queue():
+    has_been = load_p(filename=TODO_QUEUE) or set()
+    return has_been
+
+
+def set_todo_queue(urls):
+    if urls:
+        dump_p(urls, filename=TODO_QUEUE)
